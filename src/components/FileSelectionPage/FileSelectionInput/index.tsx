96af194fa3fd7e7b198ff8363457b0add203bf4b
zake-dev <zake-dev@github.com>
2020-09-11 10:57:12 +0900

    feat: file upload and validation

---
+++ src/components/FileSelectionPage/FileSelectionInput/index.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Typography, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import { StyledPaper } from "./style";
+import { ThemeContext } from "styled-components";
+
+type FileSelectionInputProps = {
+  fileName: string;
+  handleFile: (fileUploaded) => void;
+};
+
+const FileSelectionInput: React.FC<FileSelectionInputProps> = (props) => {
+  const theme = React.useContext(ThemeContext);
+  const hiddenFileInput = React.useRef(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    props.handleFile(fileUploaded);
+  };
+
+  return (
+    <StyledPaper elevation={0} variant="outlined">
+      <IconButton style={{ padding: 8 }} onClick={handleClick}>
+        <SearchOutlined style={{ color: theme.color.main }} />
+      </IconButton>
+      <Typography>{props.fileName}</Typography>
+      <input
+        type="file"
+        accept=".xls, .xlsx"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        style={{ display: "none" }}
+      />
+    </StyledPaper>
+  );
+};
+
+export default FileSelectionInput;
--- src/components/FileSelectionPage/index.tsx
+++ src/components/FileSelectionPage/index.tsx
@@ -1,20 +1,52 @@
 import * as React from "react";
+import { Snackbar } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
 import MainAppBar from "components/MainAppBar";
 import {
   FileSelectionPageContainer,
   FileSelectionBox,
   StyledButton,
 } from "./style";
-import FileSelectionTextField from "./FileSelectionTextField";
+import FileSelectionInput from "./FileSelectionInput";
 
 const FileSelectionPage: React.FC = () => {
+  const [file, setFile] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleFile = (fileUploaded) => {
+    setFile(fileUploaded);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    /* Validation */
+    const extension = file?.name.split(".")[1];
+    console.log(extension);
+    if (!file || (extension !== "xls" && extension !== "xlsx")) {
+      setOpen(true);
+    }
+  };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") return;
+    setOpen(false);
+  };
+
   return (
     <FileSelectionPageContainer>
       <MainAppBar />
       <FileSelectionBox>
-        <FileSelectionTextField />
-        <StyledButton>도서검색</StyledButton>
+        <FileSelectionInput
+          fileName={file ? file.name : "Excel 파일을 불러와주세요..."}
+          handleFile={handleFile}
+        />
+        <StyledButton onClick={handleClick}>도서검색</StyledButton>
       </FileSelectionBox>
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">
+          Excel파일을 업로드해주세요!
+        </Alert>
+      </Snackbar>
     </FileSelectionPageContainer>
   );
 };
