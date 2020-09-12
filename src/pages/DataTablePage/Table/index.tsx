67327f8835739eff9c98e5872a46c357d0f44df6
zake-dev <zake-dev@github.com>
2020-09-12 18:06:00 +0900

    fix: customized mui-datatables

--- src/pages/DataTablePage/Table/index.tsx
+++ src/pages/DataTablePage/Table/index.tsx
@@ -30,7 +30,17 @@ export const Table: React.FC<Props> = (props) => {
   ];
 
   const options = {
+    elevation: 0,
+    filter: false,
+    print: false,
+    viewColumns: false,
     filterType: "checkbox",
+    downloadOptions: { filename: "도서 견적서.csv" },
+    onDownload: (buildHead, buildBody, columns, data) => {
+      return "\uFEFF" + buildHead(columns) + buildBody(data);
+    },
+    responsive: "scrollMaxHeight",
+    tableBodyHeight: "60vh",
   };
 
   return (
@@ -40,5 +50,6 @@ export const Table: React.FC<Props> = (props) => {
       columns={columns}
       options={options}
     />
+    // </MuiThemeProvider>
   );
 };
--- src/pages/DataTablePage/index.tsx
+++ src/pages/DataTablePage/index.tsx
@@ -1,22 +1,27 @@
 import * as React from "react";
-import { Typography } from "@material-ui/core";
-import { useLocation } from "react-router-dom";
+import { RouteComponentProps, useLocation } from "react-router-dom";
 import MainAppBar from "components/MainAppBar";
 import { PageWrapper, ContentWrapper } from "components/Wrappers";
 import { Book } from "services/";
 import { Table } from "./Table";
+import { StyledButton } from "./styles";
 
-interface Props {}
+interface Props extends RouteComponentProps {}
 
-export const DataTablePage: React.FC<Props> = (props) => {
+export const DataTablePage: React.FC<Props> = ({ history }) => {
   const location = useLocation();
   const books: Book[] = location.state["books"];
+
+  const handlePushToHome = (event: React.MouseEvent<HTMLElement>) => {
+    history.push("/");
+  };
 
   return (
     <PageWrapper>
       <MainAppBar />
       <ContentWrapper>
         <Table books={books} />
+        <StyledButton onClick={handlePushToHome}>처음으로</StyledButton>
       </ContentWrapper>
     </PageWrapper>
   );
